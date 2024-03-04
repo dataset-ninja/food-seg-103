03d2df18c225d00fc54040f3830b72f39a3f3841
peixed351 <peixed351@gmail.com>
2024-03-04 18:41:47 +0300

    some check

--- src/settings.py
+++ src/settings.py
@@ -36,7 +36,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://xiongweiwu.github.io/foodseg103.html"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 14418685
+PREVIEW_IMAGE_ID: int = 14689177
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/food-seg-103"
